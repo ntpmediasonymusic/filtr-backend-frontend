c87d822f145cfb904f50e013200490610b5c5f23
David Six <dsix@inetu.net>
2016-08-31 12:35:05 +0000

    Update wp-config for DeSMan-only use

--- config/wp-config.php
+++ config/wp-config.php
@@ -20,90 +20,15 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-if ( getenv('DESMAN_ENV') ) {
-  if ( getenv('DESMAN_CONTAINERIZER') == 'docker' ) {
-    define('DB_NAME',getenv('DESMAN_DB_ENV_MYSQL_DATABASE') );
-    define('DB_USER',getenv('DESMAN_DB_ENV_MYSQL_USER') );
-    define('DB_PASSWORD',getenv('DESMAN_DB_ENV_MYSQL_PASSWORD') );
-    define("DB_PORT" , getenv("DESMAN_DB_PORT_3306_TCP_PORT") ?: 3306 );
-    define('DB_HOST',getenv('DESMAN_DB_PORT_3306_TCP_ADDR') . ":" . DB_PORT );
-  } else {
-    require_once(sprintf("%s/.dbc.php",dirname($_SERVER['DOCUMENT_ROOT'])));
-  }
-  require_once(sprintf("%s/repo/.inetu/salt.php",dirname($_SERVER['DOCUMENT_ROOT'])));
-} else {
-  // ** MySQL settings - You can get this info from your web host ** //
-  /** The name of the database for WordPress */
-  define('DB_NAME', getenv('DESMAN_MYSQL_DB_NAME'));
+define('DB_NAME',getenv('DESMAN_DB_ENV_MYSQL_DATABASE') );
+define('DB_USER',getenv('DESMAN_DB_ENV_MYSQL_USER') );
+define('DB_PASSWORD',getenv('DESMAN_DB_ENV_MYSQL_PASSWORD') );
+define("DB_PORT" , getenv("DESMAN_DB_PORT_3306_TCP_PORT") ?: 3306 );
+define('DB_HOST',getenv('DESMAN_DB_PORT_3306_TCP_ADDR') . ":" . DB_PORT );
 
-  /** MySQL database username */
-  define('DB_USER', getenv('DESMAN_MYSQL_DB_USERNAME'));
+/** Salts from docker build */
+require_once(sprintf("%s/salt.php",dirname($_SERVER['DOCUMENT_ROOT'])));
 
-  /** MySQL database password */
-  define('DB_PASSWORD', getenv('DESMAN_MYSQL_DB_PASSWORD'));
-
-  /** MySQL hostname within Openshift. */
-  define('DB_HOST', getenv('DESMAN_MYSQL_DB_HOST') . ':' . getenv('DESMAN_MYSQL_DB_PORT'));
-
-  /**#@+
-   * Authentication Unique Keys and Salts.
-   *
-   * Change these to different unique phrases!
-   * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
-   * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
-   *
-   * @since 2.6.0
-   */
-
-  // This is where we define the OpenShift specific secure variable functions
-  require_once(getenv('OPENSHIFT_REPO_DIR') . '.openshift/openshift.inc');
-
-  // Set the default keys to use
-  $_default_keys = array(
-    'AUTH_KEY'          => ' w*lE&r=t-;!|rhdx5}vlF+b=+D>a)R:nTY1Kdrw[~1,xDQS]L&PA%uyZ2:w6#ec',
-    'SECURE_AUTH_KEY'   => '}Sd%ePgS5R[KwDxdBt56(DM:0m1^4)-k6_p8}|C:[-ei:&qA)j!X`:7d-krLZM*5',
-    'LOGGED_IN_KEY'     => '$l^J?o)!zhp6s[-x^ckF}|BjU4d+(g1as)n/Q^s+k|,ZZc@E^h%Rx@VTm|0|?]6R',
-    'NONCE_KEY'         => '#f^JM8d^!sVsq]~|4flCZHdaTy.-I.f+1tc[!h?%-+]U}|_8qc K=k;]mXePl-4v',
-    'AUTH_SALT'         => 'I_wL2t!|mSw_z_ zyIY:q6{IHw:R1yTPAO^%!5,*bF5^VX`5aO4]D=mtu~6]d}K?',
-    'SECURE_AUTH_SALT'  => '&%j?6!d<3IR%L[@iz=^OH!oHRXs4W|D,VCD7w%TC.uUa`NpOH_XXpGtL$A]{+pv9',
-    'LOGGED_IN_SALT'    => 'N<mft[~OZp0&Sn#t(IK2px0{KloRcjvIJ1+]:,Ye]>tb*_aM8P&2-bU~_Z>L/n(k',
-    'NONCE_SALT'        => 'u E-DQw%[k7l8SX=fsAVT@|_U/~_CUZesq{v(=y2}#X&lTRL{uOVzw6b!]`frTQ|'
-  );
-
-  // This function gets called by openshift_secure and passes an array
-  function make_secure_key($args) {
-    $hash = $args['hash'];
-    $key  = $args['variable'];
-    $original = $args['original'];
-
-    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    $chars .= '!@#$%^&*()';
-    $chars .= '-_ []{}<>~`+=,.;:/?|';
-
-    // Convert the hash to an int to seed the RNG
-    srand(hexdec(substr($hash,0,8)));
-    // Create a random string the same length as the default
-    $val = '';
-    for($i = 1; $i <= strlen($original); $i++){
-      $val .= substr( $chars, rand(0,strlen($chars))-1, 1);
-    }
-    // Reset the RNG
-    srand();
-    // Set the value
-    return $val;
-  }
-
-  // Generate OpenShift secure keys (or return defaults if not on OpenShift)
-  $array = openshift_secure($_default_keys,'make_secure_key');
-
-  // Loop through returned values and define them
-  foreach ($array as $key => $value) {
-    define($key,$value);
-  }
-
-  /**#@-*/
-
-}
 // ** Object storage settings ** //
 define( 'DESMAN_OBS_KEY_ID', getenv('DESMAN_OBS_KEY_ID'));
 define( 'DESMAN_OBS_KEY_SECRET', getenv('DESMAN_OBS_KEY_SECRET'));
@@ -143,7 +68,7 @@ define('WP_DEBUG', false);
 define('FORCE_SSL_ADMIN', true);
 
 /* Disable plugin install in prod */
-define( 'DISALLOW_FILE_MODS', (getenv('OPENSHIFT_DEPLOYMENT_BRANCH') ?: getenv("DESMAN_ENV")) == 'prod');
+define( 'DISALLOW_FILE_MODS', getenv("DESMAN_ENV") == 'prod');
 
 /* That's all, stop editing! Happy blogging. */
 
@@ -151,7 +76,7 @@ define( 'DISALLOW_FILE_MODS', (getenv('OPENSHIFT_DEPLOYMENT_BRANCH') ?: getenv("
 if ( !defined('ABSPATH') )
   define('ABSPATH', dirname(__FILE__) . '/');
 
-if ( (getenv("OPENSHIFT_DEPLOYMENT_BRANCH") ?: getenv("DESMAN_ENV")) == "devel" ) define('SCRIPT_DEBUG',true);
+if ( getenv("DESMAN_ENV") == "devel" ) define('SCRIPT_DEBUG',true);
 $scheme = $_SERVER['HTTP_X_FORWARDED_PROTO'] ?: 'http';
 define('WP_HOME',$scheme."://".$_SERVER['HTTP_HOST']);
 define('WP_SITEURL',WP_HOME);
